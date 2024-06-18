2446a6dcd8d7c71f6db57c2a69937a666b4eb8a0
Anja <anja@jesierski.de>
2024-06-18 11:05:19 +0200

    Fix .gitignore

---
+++ c_sharp/Alterskontrolle/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Wie heißt du?");
+        string Name = Console.ReadLine();
+        Console.WriteLine("Wie alt bist du?");
+        string Alter = Console.ReadLine();
+        int alt = Int32.Parse(Alter);
+        if (alt >= 18)
+        {
+            Console.WriteLine(Name + ", du darfst fahren.");
+            if (alt <= 21)
+                Console.WriteLine("Es gibt jedoch einige Einschränkungen, die du beachten musst.");
+        }
+        else
+        {
+            Console.WriteLine(Name + ", du bist noch zu jung, um zu fahren.");
+            int diff = 18 - alt;
+            if (diff == 1)
+            {
+                Console.WriteLine("Warte noch " + diff.ToString() + " Jahr.");
+            }
+            else {
+                Console.WriteLine("Warte noch " + diff.ToString() + " Jahre.");
+            }
+        }
+    }
+}
