d1a3fa1d30a2a5b76edf319e887580c191c786dc
Anja <anja@jesierski.de>
2024-06-19 16:57:43 +0200

    Add game ZahlRaten

---
+++ c_sharp/ZahlRaten/Program.cs
@@ -0,0 +1,18 @@
+﻿// Zahl raten
+Random random = new Random();
+int Zahl = 0;
+string Eingabe = "";
+int Zufall = random.Next(1, 1000);
+Console.WriteLine("########## Ratespiel ##########");
+while (Zahl != Zufall)
+{
+    Console.WriteLine("Rate eine Zahl: ");
+    Eingabe = Console.ReadLine();
+    Zahl = int.Parse(Eingabe);
+    if (Zahl < Zufall) Eingabe = Eingabe + " ist zu klein.";
+    if (Zahl > Zufall) Eingabe = Eingabe + " ist zu groß.";
+    if (Zahl == Zufall) Eingabe = Eingabe + " ist richtig!";
+    Console.WriteLine(Eingabe);
+}
+Console.WriteLine("Die Zahl ist " + Zufall);
+
