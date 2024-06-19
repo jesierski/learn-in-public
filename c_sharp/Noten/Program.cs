c418092141061d866e88a36ff6faa9e6cf0bbe8e
Anja <anja@jesierski.de>
2024-06-19 15:57:22 +0200

    Add Noten

---
+++ c_sharp/Noten/Program.cs
@@ -0,0 +1,25 @@
+﻿// Zensur
+Console.WriteLine("Gib eine Note von 1 bis 6 ein: ");
+string Note = Console.ReadLine();
+int Wert = int.Parse(Note);
+if (Wert == 1) Note = "Sehr gut";
+if (Wert == 2) Note = "Gut";
+if (Wert == 3) Note = "Befriedigend";
+if (Wert == 4) Note = "Ausreichend";
+if (Wert == 5) Note = "Mangelhaft";
+if (Wert == 6) Note = "Ungenügend";
+Console.WriteLine(Note);
+
+Console.WriteLine("Oder über switch-case: ");
+string Note2 = Console.ReadLine();
+int Wert2 = int.Parse(Note2);
+switch (Wert2)
+{ 
+    case 1: Note2 = "Sehr gut"; break;
+    case 2: Note2 = "Gut"; break;
+    case 3: Note2 = "Befriedigend"; break;
+    case 4: Note2 = "Ausreichend"; break;
+    case 5: Note2 = "Mangehaft"; break;
+    case 6: Note2 = "Ungenügend"; break;
+}
+Console.WriteLine(Note2);
