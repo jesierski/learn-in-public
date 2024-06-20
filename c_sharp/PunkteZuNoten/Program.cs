1d89a61c9d94c9843b93d2cbaf9943d271159dc3
Anja <anja@jesierski.de>
2024-06-20 09:08:57 +0200

    Start PunkteZuNoten

---
+++ c_sharp/PunkteZuNoten/Program.cs
@@ -0,0 +1,8 @@
+﻿// Punkteingaben zwischen 0 und 100 werden zu Noten umgerechnet
+Console.WriteLine("Bitte geben Sie eine Zahl zwischen 0 und 100 ein: ");
+string Eingabe = Console.ReadLine();
+int Zahl = int.Parse(Eingabe);
+while () 
+{
+    try { } catch { }
+}
