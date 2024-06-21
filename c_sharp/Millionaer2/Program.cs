95e43fb13af866cf04a4c364204a31bd03056110
Anja <anja@jesierski.de>
2024-06-21 11:00:48 +0200

    Add how much user owns after no of years waiting

---
+++ c_sharp/Millionaer2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Millionaer2
+
+// Deklaration der Variablen
+string Eingabe = "";
+float Zinsen = 0;
+
+// Eingaben des Nutzers
+Console.WriteLine("Wie viel Geld willst du anlegen?");
+Eingabe = Console.ReadLine();
+float Kapital = float.Parse(Eingabe);
+Console.WriteLine("Wie hoch soll der Zinssatz (mit Komma) sein?");
+Eingabe = Console.ReadLine();
+float Prozent = float.Parse(Eingabe);
+Console.WriteLine("Wie lang willst du warten?");
+Eingabe = Console.ReadLine();
+int Laufzeit = int.Parse(Eingabe);
+
+// Berechnung
+for (int Nr = 0; Nr < Laufzeit; Nr++) 
+{
+    Zinsen = Kapital * Prozent / 100;
+    Kapital = Kapital + Zinsen;
+}
+
+Console.WriteLine("Du hast " + Kapital + " Euro auf dem Konto.");
