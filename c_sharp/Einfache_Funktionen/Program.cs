d10727b958ddc4b99dee7185e30c10592c5da4f3
Anja <anja@jesierski.de>
2024-06-26 15:50:54 +0200

    Add more functions

---
+++ c_sharp/Einfache_Funktionen/Program.cs
@@ -0,0 +1,36 @@
+﻿// Addition und Multiplikation von Zahlen
+Console.WriteLine("######## Addition und Mulitplikation ###########");
+
+/*a) Schreibe eine Funktion BerechneSumme, die zwei Integer-Parameter entgegennimmt 
+ * und die Summe dieser beiden Zahlen zurückgibt. */
+
+static int BerechneSumme(int a, int b) 
+{ 
+    return a + b;
+}
+
+/*b) Schreibe eine Funktion Multipliziere, die zwei Integer-Parameter entgegennimmt 
+ * und das Produkt dieser beiden Zahlen zurückgibt. */
+
+static int Multipliziere(int a, int b) 
+{ 
+    return (a * b);
+}
+
+/*c) Erstelle eine Schleife in der Main-Methode, die von 1 bis 5 läuft. 
+ * Innerhalb der Schleife rufe die Funktion BerechneSumme auf, indem du 
+ * der Funktion die Zählervariable und die Zahl 10 übergibst. 
+ * Gib das Ergebnis jeder Berechnung auf der Konsole aus. */
+
+int count = 1;
+while (count < 6)
+{
+    int res = BerechneSumme(count, 10);
+    Console.WriteLine("Die Summe von count und 10 ist " + res + ".");
+    count++;
+}
+
+/*d) Nachdem die Schleife abgeschlossen ist, rufe die Funktion Multipliziere auf, 
+ * indem du ihr die Zahlen 5 und 10 übergibst. 
+ * Gib das Ergebnis auf der Konsole aus.  */
+Console.WriteLine("Funktion Multipliziere(5, 10): " + Multipliziere(5, 10));
