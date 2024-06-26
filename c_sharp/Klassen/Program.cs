7fbc2627f0ce07ef36f12bf23c835a1aa02c0215
Anja Jesierski <42860442+jesierski@users.noreply.github.com>
2024-06-26 17:44:42 +0200

    Use base classes (#21)

---
+++ c_sharp/Klassen/Program.cs
@@ -0,0 +1,14 @@
+﻿// Erzeuge Monster
+public class Monster
+{
+    public string Name, Wesen;
+    public Monster(string N string W)
+    {
+        Name = N; Wesen = W;
+    }
+    public void Show() 
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Wesen: " + Wesen);
+    }
+}
---
+++ c_sharp/Klassen2/Class2.cs
@@ -0,0 +1,39 @@
+﻿// Erzeuge Monster
+public class Monster
+{
+    public string Name, Wesen;
+    public Monster(string N, string W)
+    {
+        Name = N; Wesen = W;
+    }
+    public void Show()
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Wesen: " + Wesen);
+        Console.WriteLine("Typ: " + Type());
+    }
+    public virtual string Type() // class Monster
+    {
+        return("Monster");
+    }
+}
+public class GMonster : Monster 
+{
+    public GMonster(string N, string W) : base(N, W) 
+    { 
+    }
+    public override string Type() // class GMonster 
+    {
+        return("Geistes-Monster");
+    }
+}
+public class SMonster : Monster 
+{
+    public SMonster(string N, string W) : base(N, W)
+    {
+    }
+    public override string Type() // class SMonster
+    {
+        return ("Seelen-Monster");
+    }
+}
