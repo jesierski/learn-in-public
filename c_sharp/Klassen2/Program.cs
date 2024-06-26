8520e761ad6aad46b27b63cdb8b48fd6fb6b5f18
Anja <anja@jesierski.de>
2024-06-26 17:42:19 +0200

    Use base classes

---
+++ c_sharp/Klassen2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Monster
+
+// Hauptprogramm
+Monster Frank = new Monster("Frankie", "außergewöhnlich");
+Frank.Show();
+GMonster Albert = new GMonster("Bertie", "nachdenklich");
+Albert.Show();
+SMonster Sigmund = new SMonster("Sigi", "einfühlsam");
+Sigmund.Show();
