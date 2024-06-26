5237a513d01e3af7eccec2347b5dcac6641bc7c9
Anja Jesierski <42860442+jesierski@users.noreply.github.com>
2024-06-26 14:56:39 +0200

    Add more elegant version of Textanalyse (#20)

---
+++ c_sharp/TextAnalyseVer2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Sehr elegante Version 2 der Textanalyse
+var (anzahlWörter, durchschnittlicheWortlänge) = TextAnalyse("Dies ist ein Beispieltext zur Demonstration.");
+Console.WriteLine($"Anzahl der Wörter: {anzahlWörter}, Durchschnittliche Wortlänge: {durchschnittlicheWortlänge}");
+ 
+static (int, double) TextAnalyse(string text)
+{
+    string[] wörter = text.Split(new char[] { ' ', '.', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+    int anzahlWörter = wörter.Length;
+    int gesamtLänge = 0;
+    foreach (var wort in wörter)
+    {
+        gesamtLänge += wort.Length;
+    }
+    double durchschnittlicheWortlänge = (double)gesamtLänge / anzahlWörter;
+    return (anzahlWörter, durchschnittlicheWortlänge);
+} 
+
