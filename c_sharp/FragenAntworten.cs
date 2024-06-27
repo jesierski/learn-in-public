42c0633b7c26048a70ebc3e78a8e3603306e9cf2
Anja <anja@jesierski.de>
2024-06-27 14:27:17 +0200

    Cleanup QuizFktKlassen

--- c_sharp/FragenAntworten.cs
+++ c_sharp/FragenAntworten.cs
@@ -5,7 +5,7 @@ public class Class1
 	public Class1()
 	{
         // Quiz mit 5 Fragen, jede hat 4 Antworten, nur 1 richtige
-        Console.WriteLine("Hallo Quiz-Sieler!");
+        Console.WriteLine("Hallo Quiz-Spieler!");
         Console.Write("Hier kommen bis zu 5 Fragen, jede mit 4 Antworten. ");
         Console.WriteLine("Doch nur 1 ist die richtige. Weißt du welche?");
         Console.WriteLine("------------------------------------------------");
@@ -13,7 +13,7 @@ public class Class1
         String[] arrFragen = {
         "Wie heißt die Hauptstadt von Belgien? A) Amsterdam B) Paris C) Brüssel D) Luxembourg",
         "Welches ist der höchste Berg? A) Zugspitze B) Mount Everst C) Kilimandscharo D) Nanga Parbat",
-        "Wie tief ist die tieste Stelle der Erde ab Meeresspiegel? A) 11.020 m B) 9.220 m C) 8.870 m D) 4.500 m",
+        "Wie tief ist die tiefste Stelle der Erde ab Meeresspiegel? A) 11.020 m B) 9.220 m C) 8.870 m D) 4.500 m",
         "Wie viele Farben hat der Regenbogen? A) 3 B) 8 C) 7 D) 6",
         "Wie groß ist der größte Vogel der Welt? A) europäische Gans B) afrikanische Strauß C) Albatros D) Andenkondor"};
         char[] arrAntworten = { 'C', 'B', 'A', 'C', 'B' };
