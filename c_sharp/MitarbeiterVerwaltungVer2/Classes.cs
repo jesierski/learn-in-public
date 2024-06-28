e6cf5f75899ad15747c14309bec649b18a182171
Anja <anja@jesierski.de>
2024-06-28 14:41:57 +0200

    Add MitarbeiterVerwaltung enhanced by classes

---
+++ c_sharp/MitarbeiterVerwaltungVer2/Classes.cs
@@ -0,0 +1,104 @@
+﻿// Klassen für die Mitarbeiterverwaltung
+public class Mitarbeiter 
+{
+    // Attribute der Klasse Mitarbeiter
+    public int MitarbeiterID { get; set; }
+    public string Name { get; set; }
+    public string Position { get; set; }
+    public double Gehalt { get; set; }
+
+    // Konstruktor der Klasse Mitarbeiter
+    public Mitarbeiter(int mitarbeiterid, string name, string position, double gehalt)
+    {
+        MitarbeiterID = mitarbeiterid;
+        Name = name;
+        Position = position;
+        Gehalt = gehalt;
+    }
+
+    // Methoden in der Klasse Mitarbeiter
+    public void AktualisiereName(string neuerName) 
+    { 
+        Name = neuerName;
+    }
+
+    public void AktualisierePosition(string neuePosition) 
+    { 
+        Position = neuePosition;
+    }
+
+    public void AktualisiereGehalt(double neuesGehalt) 
+    { 
+        Gehalt = neuesGehalt;
+    }
+
+}
+
+public class MitarbeiterVerwaltung
+{
+    //// Attribute der  Klasse MitarbeiterVerwaltung
+    //List<Mitarbeiter> ListeMitarbeiter;
+
+    //// Konstruktor der Klasse MitarbeiterVerwaltung
+    //public MitarbeiterVerwaltung(List<Mitarbeiter> listeMitarbeiter) 
+    //{
+    //    ListeMitarbeiter = listeMitarbeiter;
+    //}
+
+    private List<Mitarbeiter> ListeMitarbeiter = new List<Mitarbeiter>();
+
+    // Methoden
+    // Hinzufügen eines Mitarbeiters
+    public void MitarbeiterHinzufuegen(Mitarbeiter mitarbeiter) 
+    {
+        ListeMitarbeiter.Add(mitarbeiter);
+    }
+
+    // Aktualisieren der Daten eines Mitarbeiters
+    public void MitarbeiterAktualisieren(int mitarbeiterid, string name, string position, double gehalt) 
+    {
+        foreach (Mitarbeiter mitarbeiter in ListeMitarbeiter) 
+        {
+            if (mitarbeiter.MitarbeiterID == mitarbeiterid)
+            {
+                mitarbeiter.Name = name;
+                mitarbeiter.Position = position;
+                mitarbeiter.Gehalt = gehalt;
+                break;
+            }
+            else 
+            {
+                Console.WriteLine("Dieser Mitarbeiter existiert nicht.");
+                break;
+            }
+        }
+    }
+
+    // Löschen eines Mitarbeiters
+    public void MitarbeiterLoeschen(int mitarbeiterid) 
+    {
+        foreach (Mitarbeiter mitarbeiter in ListeMitarbeiter.ToList()) 
+        {
+            if (mitarbeiter.MitarbeiterID == mitarbeiterid) 
+            {
+                ListeMitarbeiter.Remove(mitarbeiter);
+            }
+        }
+    }
+
+    // Alle Mitarbeiter anzeigen
+    public void MitarbeiterAnzeigen() 
+    {
+        Console.WriteLine("Alle Mitarbeiter des Unternehemens: ");
+        Console.WriteLine("----------------------------------------------------");
+        foreach (Mitarbeiter mitarbeiter in ListeMitarbeiter) 
+        {
+            Console.Write(
+                "Mitarbeiter-ID: " + mitarbeiter.MitarbeiterID + ", " +
+                "Name: " + mitarbeiter.Name + ", " +
+                "Position: " + mitarbeiter.Position + ", " +
+                "Gehalt: " + mitarbeiter.Gehalt);
+        }
+        Console.WriteLine("----------------------------------------------------");
+    }
+}
---
+++ c_sharp/MitarbeiterVerwaltungVer2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Mitarbeiterverwaltung über Konsolenanwendung
+Console.WriteLine("#################################################");
+Console.WriteLine("############# Mitarbeiterverwaltung #############");
+Console.WriteLine("#################################################");
+
+// Hauptprogramm
+// Test
+MitarbeiterVerwaltung verwaltung = new MitarbeiterVerwaltung();
+
+Mitarbeiter m1 = new Mitarbeiter(1, "Max Müller", "Chef", 3204.23);
+Mitarbeiter m2 = new Mitarbeiter(2, "Irene Müller", "Chefin", 3204.23);;
+verwaltung.MitarbeiterHinzufuegen(m1);
+verwaltung.MitarbeiterHinzufuegen(m2);
+verwaltung.MitarbeiterAnzeigen();
+Console.WriteLine("############ Aktualisieren ###########");
+verwaltung.MitarbeiterAktualisieren(1, "Max Müller", "Chef", 4500.01);
+verwaltung.MitarbeiterAnzeigen();
+Console.WriteLine("############### Löschen ###############");
+verwaltung.MitarbeiterLoeschen(1);
+verwaltung.MitarbeiterAnzeigen();
