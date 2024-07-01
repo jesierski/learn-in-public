1297cd864c82f36465e3d79afce2e3ba23e2aa4b
Anja <anja@jesierski.de>
2024-07-01 16:53:56 +0200

    Add first formular for graphics

---
+++ c_sharp/KonsoleZumFormular/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace KonsoleZumFormular
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(39, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Grüße mich";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(248, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Farbe ändern";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 146);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(248, 146);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 57);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Personalisierte Begrüßung";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Mein interaktives Fenster";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ c_sharp/KonsoleZumFormular/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KonsoleZumFormular
+{   
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Basisklasse basisklasse = new Basisklasse();
+            basisklasse.Begruessen();
+            AbgeleiteteKlasse abgKlasse = new AbgeleiteteKlasse();
+            abgKlasse.Begruessen();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private int zaehler = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Hallo, Welt!", "Grüße mich");
+            zaehler++;
+            Console.WriteLine($"Die Schaltfläche wurde {zaehler} Mal gedrückt.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int max = 256;
+            int r = random.Next(0, max);
+            int g = random.Next(0, max);
+            int b = random.Next(0, max);
+            Color color = Color.FromArgb(r, g, b);
+            BackColor = color;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string Text = textBox1.Text;
+            MessageBox.Show("Hallo, " + Text + "!", "Personalisierte Begrüßung");
+        }
+
+        public class Basisklasse
+        {
+            public virtual void Begruessen()
+            {
+                MessageBox.Show("Willkommen im interaktiven Fenster!");
+            }
+        }
+
+        public class AbgeleiteteKlasse : Basisklasse
+        {
+            public override void Begruessen()
+            {
+                MessageBox.Show("Spezielle Begrüßung!");
+            }
+        }
+    }
+
+}
