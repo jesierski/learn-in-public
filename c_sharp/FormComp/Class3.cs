c47df37d5092c695d3bff40ba9d3c0e490c64190
Anja <anja@jesierski.de>
2024-07-03 19:46:27 +0200

    Practise components, activities, encapsulation

---
+++ c_sharp/FormComp/Class3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FormComp
+{
+	internal class Basisklasse : Button
+	{
+		Basisklasse button = new Button();
+
+		internal Basisklasse()
+		{
+		}
+		public virtual void klicken() 
+		{ 
+		}
+	}
+
+	internal class AnzeigeButton : Basisklasse 
+	{
+        public override void klicken()
+        {
+            textBox1.Text = (string)listBox1.SelectedItem;
+        }
+    }
+
+	internal class HinzufuegenButton : Basisklasse 
+	{
+        public override void klicken()
+        {
+            listBox1.Items.Add(listBox1.Text);
+        }
+    }
+
+	internal class EntfernenButton : Basisklasse 
+	{
+        public override void klicken()
+        {
+            listBox1.Items.Remove(textBox1.Text);
+        }
+    }
+}
