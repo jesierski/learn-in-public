c890edee2e923f985d3c27b388e20ae43cc4ccb6
Anja Jesierski <42860442+jesierski@users.noreply.github.com>
2024-07-04 17:38:41 +0200

    Visual components (#29)

---
+++ c_sharp/Grafikprogramm/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Grafikprogramm
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
