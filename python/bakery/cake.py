50b375f78b224e576fe7d9ec16f37520beb0be8d
Anja <anja@jesierski.de>
2025-03-06 17:34:17 +0100

    add changes in current branch

---
+++ python/bakery/cake.py
@@ -0,0 +1,11 @@
+# Define Class Cake with attributes kind, price, and slices
+class Cake:
+    def __init__(self, kind, price, slices):
+        self.kind = kind
+        self.price = price
+        self.slices = slices
+
+    # Define class method describe() to print sample message:
+    # The citron cake costs 24 euros and has 6 slices.
+    def describe(self):
+        print("The " + self.kind + " cake costs " + self.price + " euros and has " + self.slices + " pieces.")
