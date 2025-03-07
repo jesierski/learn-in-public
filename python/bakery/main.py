c4cfc6cc02889df8d7d9032817b861b797f32afd
Anja Jesierski <anja@jesierski.de>
2025-03-07 18:48:51 +0100

    track sold pieces of cake

--- python/bakery/main.py
+++ python/bakery/main.py
@@ -1,7 +1,4 @@
-# Define Class Cake with attributes kind, price, and slices
-from pydoc import describe
-
-
+# Define Class Cake with attributes kind, price, slices in total and remaining pieces
 class Cake:
     # Constructor for class Cake
     def __init__(self, kind, price, slices):
@@ -11,13 +8,27 @@ class Cake:
         self.price = price
         assert isinstance(slices, int)
         self.slices = slices
+        self.slices_remaining = slices
 
     # Define class method describe() to print message.
     # Sample: The citron cake costs 24 euros and has 6 slices.
     def describe(self):
         return f"The {self.kind} cake costs {self.price} euros and has {self.slices} pieces."
 
+    # Deduct the sold amount of one cake count of the remaining pieces
+    # provided conditions are met
+    def sell(self, count):
+        if (count <= 0):
+            return "Selling of 0 or a negative count not possible!"
+        elif (self.slices_remaining-count < 0):
+            return f"Selling of more than existing pieces ({self.slices_remaining}) not possible!"
+        else:
+            self.slices_remaining -= count
+            return f"With this cake there are {self.slices_remaining} pieces remaining."
+
 spice_cake = Cake("Gewürz", 18, 8)
 chocolate_cake = Cake("Schokoladen", 24, 6)
-print(Cake.describe(spice_cake))
-print(Cake.describe(chocolate_cake))
+print(spice_cake.sell(5))
+print(spice_cake.sell(4))
+print(chocolate_cake.sell(-1))
+print(chocolate_cake.sell(0))
