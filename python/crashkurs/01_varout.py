3a68a77e0c9f6159fb0ca3c1ce48589bf1d26d99
Anja Jesierski <anja@jesierski.de>
2025-03-12 09:43:15 +0100

    comparison operators

--- python/crashkurs/01_varout.py
+++ python/crashkurs/01_varout.py
@@ -1,6 +1,7 @@
 # Int, floats, str, boolean
 w = 2
 x = 5.0
+x2 = 5.2
 y = "Hallo"
 z = True
 
@@ -13,4 +14,29 @@ print(x, z)
 b1 = True
 b2 = False
 b3 = b1 + b2
+
+# Print boolean to console
 print (b3)
+
+# Use operators + (-, *, /)
+c = w + x
+print(c)
+
+# Comparison and negation ==, !=, <, <= , >, >=, and, or
+print(w == x)
+print(w != x)
+print(w < x)
+print(w <= x)
+print(w > x)
+print(w >= x)
+print(w is x)
+print(w is not x)
+# Gives error as float and str cannot be compared
+#print(w < x and x < y)
+print(w < x and x < x2)
+print(w < x < x2)
+#print(w < x or x < y)
+print(w < x or x < x2)
+#print(not (w < x and x < y))
+print(not (w < x and x < x2))
+print(not (w < x < x2))
