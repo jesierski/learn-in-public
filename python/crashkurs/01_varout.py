8359c529e11211b035d7fe440e7393053e9de612
Anja Jesierski <anja@jesierski.de>
2025-03-12 08:57:22 +0100

    add variables and their output

---
+++ python/crashkurs/01_varout.py
@@ -0,0 +1,16 @@
+# Int, floats, str, boolean
+w = 2
+x = 5.0
+y = "Hallo"
+z = True
+
+# Print variables to console
+print(w, x, y, z)
+print(x, y, z, end="|", sep=" - ")
+print(x, z)
+
+# Boolean are handled as 1 and 0 internally
+b1 = True
+b2 = False
+b3 = b1 + b2
+print (b3)
